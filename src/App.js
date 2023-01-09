a0fd5b814ecfc87ed8954b7af16365fa50553cf3
Tyler Watson <tylerww91@gmail.com>
2023-01-09 13:13:30 -0800

    sets background image on <main> element

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Main from './components/Main/Main';
+import background from './background.png';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundImage: `url(${background})` }}>
       <Header />
       <Main />
       <Footer />
