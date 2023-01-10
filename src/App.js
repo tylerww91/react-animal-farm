9de99dbc59c0eadae0a7da18bc7054a6693fee2b
Tyler Watson <tylerww91@gmail.com>
2023-01-09 16:56:11 -0800

    actually imports background image into Main with this commit, adjusts main height

--- src/App.js
+++ src/App.js
@@ -5,11 +5,10 @@ import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Main from './components/Main/Main';
-import background from './background.png';
 
 function App() {
   return (
-    <div className="App" style={{ backgroundImage: `url(${background})` }}>
+    <div className="App">
       <Header />
       <Main />
       <Footer />
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,10 +2,11 @@ import React from 'react';
 import './Main.css';
 import Animal from '../Animal/Animal';
 import { animals } from '../../data';
+import background from '../../background.png';
 
 export default function Main(props) {
   return (
-    <main>
+    <main style={{ backgroundImage: `url(${background})` }}>
       {animals.map((animal) => (
         <Animal
           key={animal.name}
